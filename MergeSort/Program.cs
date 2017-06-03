001e4a24d50d30d8b4cd00386898eab66b34b2a0
ALEX MARTINEZ <martineza79@outlook.com>
2017-06-02 22:03:52 -0400

    Add project files.

---
+++ MergeSort/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithm;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestMergeSort();
+
+            Console.ReadLine();
+        }
+
+        private static void TestMergeSort()
+        {
+            int[] numbers = { 105, 86, 77, 98, 1000, 1, 0, 3 };
+            numbers.DoMergeSort();
+
+            foreach (int number in numbers)
+                Console.Write(number + " ");
+        }
+    }
+}
